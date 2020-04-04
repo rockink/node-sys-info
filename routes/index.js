bca406b180cc939f6a72889c830961d9ca5f3cd1
Nirmal Nepal <rockink_nirmal@live.com>
2020-04-04 17:45:03 -0400

    adding os-info, which seems more helpful than hardware info

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,15 @@ const si = require('systeminformation');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  Promise.all([si.cpu(), si.mem()])
+  Promise.all(si.mem(), si.osInfo()])
   .then(data => 
     res.send({
-      cpu: data[0],
-      memory: data[1]
+      memory: data[0],
+      os: {
+        platform: data[1].platform,
+        distro: data[1].distro,
+        hostname: data[1].hostname
+      }
     })
   )
 });
