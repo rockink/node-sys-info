4477b88fabff0fa4743df561738943bc273defc6
Nirmal Nepal <rockink_nirmal@live.com>
2020-04-04 18:56:29 -0400

    added service to expose to outside;  also fixed the bug in the app

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const si = require('systeminformation');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  Promise.all(si.mem(), si.osInfo()])
+  Promise.all([si.mem(), si.osInfo()])
   .then(data => 
     res.send({
       memory: data[0],
